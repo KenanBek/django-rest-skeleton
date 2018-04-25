3feed0b2f841a256028c424c33c05ae7cb0b0bd4
Kenan Bek <mail@kenanbek.me>
2018-04-25 23:20:03 +0400

    Base mode for demo app

--- src/demo/admin.py
+++ src/demo/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from demo import models
 
-# Register your models here.
+admin.site.register(models.Category)
+admin.site.register(models.Tag)
+admin.site.register(models.Post)
--- src/demo/models.py
+++ src/demo/models.py
@@ -3,4 +3,29 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    posted = models.DateTimeField(auto_created=True)
+    text = models.CharField(max_length=200)
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ('posted',)
