4a05f55e528f35f29ad2288416e3b6754addba59
Kenan Bek <mail@kenanbek.me>
2018-04-25 23:20:12 +0400

    DRF configuration

--- src/drs/settings.py
+++ src/drs/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'demo',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- src/drs/urls.py
+++ src/drs/urls.py
@@ -22,6 +22,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^demo/', include('demo.urls')),
+    url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
 
