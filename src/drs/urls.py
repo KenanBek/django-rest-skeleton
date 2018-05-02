2ed7026df33b6722ccb9b40468972449c5ec531b
Kenan Bek <mail@kenanbek.me>
2018-05-03 00:19:11 +0400

    URL configuration for docs and chemas

--- src/drs/urls.py
+++ src/drs/urls.py
@@ -18,10 +18,15 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.utils.translation import ugettext as _
 
 from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
 
 from demo import api_views as demo_api_views
+
+schema_view = get_schema_view(title=_('DRS API Schema'))
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
@@ -30,11 +35,23 @@ router.register(r'posts', demo_api_views.PostViewSet)
 router.register(r'tags', demo_api_views.TagViewSet)
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(router.urls)),
-    url(r'^demo/', include('demo.urls')),
-    url(r'^api-auth/', include('rest_framework.urls')),
+    # Static home page with links (see settings.TEMPLATES conf)
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    # Link to administration
+    url(r'^admin/', admin.site.urls),
+    # Link to api
+    url(r'^api/', include(router.urls)),
+    # Link to api auth
+    url(r'^api-auth/', include('rest_framework.urls')),
+    # Link to api docs
+    url(r'^docs/', include_docs_urls(
+        title=_('DRS API'),  # Title for the doc website
+        description=_('RESTful API for DRS')  # Desc for the doc website
+    )),
+    # Link to api schema
+    url(r'^schemas/$', schema_view),
+    # Link to demo application
+    url(r'^demo/', include('demo.urls')),
 ]
 
 if settings.DEBUG:
