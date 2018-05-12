f9eaa5b3d667c78b32cb41df0420dc2c96fdde53
Kenan Rahimov <kenanbek@Kenans-MacBook-Air.local>
2018-05-12 23:47:49 +0400

    Test for tag model

--- src/demo/tests/test_models.py
+++ src/demo/tests/test_models.py
@@ -12,3 +12,27 @@ class CategoryTests(TestCase):
     def test_count(self):
         count = models.Category.objects.count()
         self.assertEqual(count, 10)
+
+
+class TagTests(TestCase):
+    def setUp(self):
+        for i in range(10):
+            tag = models.Tag()
+            tag.title = 'tag{}'.format(i)
+            tag.save()
+
+    def test_count(self):
+        count = models.Tag.objects.count()
+        self.assertEqual(count, 10)
+
+
+class PostTests(TestCase):
+    def setUp(self):
+        for i in range(10):
+            post = models.Post()
+            post.title = 'post{}'.format(i)
+            post.save()
+
+    def test_count(self):
+        count = models.Post.objects.count()
+        self.assertEqual(count, 10)
